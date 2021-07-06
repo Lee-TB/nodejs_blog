42ba784bcb528762a92c30d3d647b43315b7c529
Lee TB <tblyduc2412@gmail.com>
2021-07-06 14:53:06 +0700

    second commit

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    var a = 1
+    return res.send('Hello World!')
 })
 
 app.listen(port, () => {
